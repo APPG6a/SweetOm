c6870c529580983de8f6ba220aaa675c9ac0db84
DEMBO77 <toedembo@gmail.com>
2017-11-27 15:35:02 +0000

    minor Update View

--- View/loadUser.php
+++ View/loadUser.php
@@ -1,29 +1,30 @@
 <?php 
 $title= "messenger";
 ob_start()?>
-	<div id=CorpsloadUser.php>
+	<div id=CorpsLoadUser.php>
 		<!--vérifier si Nb_habt bien arrivé par la méthode POST-->
 		<?php
 		$_SESSION["superficie"]=$_POST["supreficie"];
-		$_SESSION["nb-habt"]=$_POST["nb-habt"];
-		$_SESSION["nb-chambre"]=$_POST["nb-chambre"];
-		$_SESSION["nb-toilette"]=$_POST["nb-toilette"];
-		$_SESSION["nb-sejour"]=$_POST["nb-sejour"];
+		$_SESSION["nbrHabt"]=$_POST["nbrHabt"];
+		$_SESSION["nbrChambre"]=$_POST["nbrChambre"];
+		$_SESSION["nbrToilette"]=$_POST["nbrToilette"];
+		$_SESSION["nbrSejour"]=$_POST["nbrSejour"];
 		?>
 
 
-		<form id="form_inscription" aaction="myHouse.php">
+		<form id="form-inscription" aaction="myHouse.php">
 			<div id=tete_form></div>
 			<p>Pour pouvoir permettre la bonne gestion des pièces veuillez inscrire ci dessous le(s) nom(s) des différents habitants.</p>
 			<?php 
 				$i=0;
-				while ($i!=$_POST["Nb_habt"])
+				while ($i!=$_POST["nbrHabt"])
 				{
-					echo "<input type=\"text\" name=\"habitant ".$i+1."\"> <label for= \"habitant ".$i+1."\"> habitant ".$i+1."</label> <br>";
+					echo "<input type=\"text\" name=habitant ".$i+1."> 
+					<label for=habitant ".$i+1."/> habitant ".$i+1."</label> <br>";
 					$i=$i+1;
 				}
 					
-				echo"<input type=\"submit\" name=\"submit\" value=\"soumettre\">";
+				echo"<input type=\"submit\" name=\"submit\" value=\"soumettre\"/>";
 			?>
 		</form>	
 	</div>
